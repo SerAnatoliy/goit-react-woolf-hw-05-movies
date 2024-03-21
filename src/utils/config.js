2c1b78bceb7627e7adbbff4846c34c0010b1277e
SerAnatoliy <anatolii@maximustribe.com>
2024-03-21 21:29:32 +0100

    main

---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const ACCESS_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTEyNWYzNWZlZDhiZTgwMDE1MjFmYmM0ZDFjMWVmOCIsInN1YiI6IjY1ZmM5NmE5MTk3ZGU0MDE4NjE0YjRjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.On3GvzvjRikdcq2Q36daP21qTQjUrrPeY2i7seSO-TY';
+
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
